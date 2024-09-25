42ac70c6d88c1fba6bff8735fbdd71f180b7200d
Olivia Kaye Minier <ominier@purdue.edu>
2024-09-25 13:50:01 -0400

    final code

--- CGT 215 Lab 5/CGT 215 Lab 5.cpp
+++ CGT 215 Lab 5/CGT 215 Lab 5.cpp
@@ -2,11 +2,48 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const std::vector<char> cypherTable = { 'V', 'F', 'X', 'B', 'L', 'I', 'T', 'Z', 'J', 'R', 'P', 'H', 'D', 'K', 'N', 'O', 'W', 'S', 'G', 'U', 'Y', 'Q', 'M', 'A', 'C', 'E' };
+
+void translateText(std::vector<int>& input) {
+	for (size_t i = 0; i < input.size(); ++i) {
+		int c = input[i];
+
+		if (c >= 65 && c <= 90) {
+			input[i] = cypherTable[c - 65];
+		}
+		else if (c >= 97 && c <= 122) {
+			int upperCaseLetter = c - 32;
+			char upperCaseCode = cypherTable[upperCaseLetter - 65];
+			input[i] = upperCaseCode + 32;
+		}
+	}
 }
+
+int main() {
+	std::vector<int> text;
+	char ch;
+	int count = 0;
+
+	std::cout << "Input text to cypher: ";
+	while (count < 128 && std::cin.get(ch) && ch != '\n') {
+		text.push_back(static_cast<int>(ch));
+		count++;
+	}
+
+	translateText(text);
+
+	std::cout << "Encoded Message: ";
+	for (int c : text) {
+		std::cout << static_cast<char>(c);
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
